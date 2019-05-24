0eebd0f24dd388bf93fa94bc733fa6e2e68564d0
Shu Chan <shuchan@Shus-MacBook-Pro.local>
2019-05-24 10:07:35 -0400

    Mutations and queries hooked up to front end

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,48 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    //TODO: check if they are logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    console.log(item);
+    return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    //first take a copy of the updates
+
+    const updates = { ...args };
+    //remove the id from the updates
+    delete updates.id;
+    //run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+
+    //1. find the item
+
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    //2. check if they own that item, or have the permissions
+    //3. Delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
+};
+
+module.exports = Mutations;
