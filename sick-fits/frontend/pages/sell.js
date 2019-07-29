a6ca0d33aa1358400152d5eef5a429074a3cdf59
ricky <rickysychan@gmail.com>
2019-07-29 07:33:52 -0400

    added please signin wrapper component

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,9 +1,11 @@
-import CreateItem from '../components/CreateItem'
-
+import CreateItem from "../components/CreateItem";
+import PleaseSignIn from "../components/PleaseSignin";
 const Sell = props => (
-    <div>
-        <CreateItem />
-    </div>
-)
+  <div>
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
+  </div>
+);
 
-export default Sell
+export default Sell;
